1cc45f4715fb6fd806c61a493e64c7f57422d242
Murillo Cesar <murilloc@gmail.com>
2022-03-12 11:03:32 -0300

    adicionando  uma implementação para getWeightKg method

--- src/com/packt/interfaces/Truck.java
+++ src/com/packt/interfaces/Truck.java
@@ -2,4 +2,19 @@ package com.packt.interfaces;
 
 public interface Truck extends Vehicle {
     int getPayloadPounds();
+
+    default int getPayloadKg() {
+        return (int) Math.round(0.454 * getPayloadPounds());
+    }
+
+    static int convertKgToPounds(int kilograms) {
+        return (int) Math.round(2.205 * kilograms);
+    }
+
+    default int getWeightKg(int pounds) {
+        return (int) Math.round(0.454 * pounds);
+    }
+
+
+
 }
